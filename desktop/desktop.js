ab414dd247f8c2298dd422eed2603a188ac22ac8
wastebin <31770725+wastebin@users.noreply.github.com>
2018-02-08 13:08:20 -0600

    Update desktop.js

--- desktop/desktop.js
+++ desktop/desktop.js
@@ -1,5 +1,30 @@
+function *run(compositor, program) {
+  let quit = false;
+  while (!quit) {
+    let [which, e] = yield;
+    switch (which) {
+      "compositor":
+        break;
+      "program":
+        break;
+    }
+  }
+}
+
 function main() {
-  spawn("display.js");
+  const compositor = new Worker("compositor.js");
+  const program = new Worker("hello.js");
+
+  const runner = run(compositor, program);
+
+  runner.next();
+
+  compositor.addEventListener("message", function(e) {
+    runner.next("compositor", e);
+  });
+  program.addEventListener("message", function(e) {
+    runner.next("program", e);
+  });
 }
 
 addEventListener("load", main);
